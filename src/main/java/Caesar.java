299ffe79179e3d05f0d0d04bfa5ab5a3d091a48d
Kiran-O <22kokelly@athenian.org>
2020-09-23 01:31:37 +0000

    Caesar Cipher Passed All Tests Still Working On Vigenere

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,54 +2,112 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        String ytr = "";
+        int shift = 3;
+        int zero = 0;
+        String ynk = "";
         for (int k = 0; k < message.length(); k++){
             char qwe = message.charAt(k);
             if (65 <= qwe && qwe <= 90){
-                qwe = (char) ((qwe +3));
+                zero = (qwe + shift - 65)%26;
+                ynk += (char) (zero + 65);
 
             }
-            else if (97 <= qwe && qwe <= 90){
-                qwe = (char) ((qwe+3));
+            else if (97 <= qwe && qwe <= 122){
+                zero = (qwe + shift - 97)%26;
+                ynk += (char) (zero +97);
                 
 
             }
-            ytr = ytr + qwe;
+            else {
+                ynk += qwe;
+            }
             
 
         }
-        return ytr;
+        return ynk;
         
         
     }
 
     public static String decryptCaesar(String message) {
-        String ytr = "";
+        int shift = 3;
+        int zero = 0;
+        String ynk = "";
         for (int k = 0; k < message.length(); k++){
             char qwe = message.charAt(k);
-            if (65 <= qwe && qwe <= 90){
-                qwe = (char) ((qwe)-3);
-
-            }
-            else if (97 <= qwe && qwe <= 90){
-                qwe = (char) ((qwe)-3);
+            if ((65 <= qwe && qwe <= 90)||(97 <= qwe && qwe <= 122)){
+                if ((65 <= qwe && qwe <= 67)||(97 <= qwe && qwe <= 99)){
+                    ynk += (char) (qwe + 23);
+                }
+                else {
+                    ynk += (char) (qwe - 3);
+                }
+                
                 
 
             }
-            ytr = ytr + qwe;
+            
+            else {
+                ynk += qwe;
+            }
+            
+
         }
-        return(ytr);
+        return ynk;
         
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String jkl = "";
+        key = key%26;
+        for (int k =0; k < message.length(); k++){
+            char qax = message.charAt(k);
+            if ((65 <= qax && qax <= 90)||(97 <= qax && qax <= 122)){
+                if ((qax >= (90-(key-1)) && qax <= 90)||(qax >= 122-(key-1) && qax <= 122)){
+                    jkl += (char) (qax-(26-key));
+                }
+                else {
+                    jkl += (char) (qax + key);
+                }
+            }
+            else {
+                jkl += qax;
+            }
+        }
+        return jkl;
+        
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int shift = key;
+        int zero = 0;
+        String wab = "";
+        for (int k = 0; k < message.length(); k++){
+            char pou = message.charAt(k);
+            if (65 <= pou && pou <= 90){
+                zero = (pou - shift - 65)%26;
+                if (zero < 0){
+                    zero = 26 + zero;
+                }
+                wab += (char) (zero + 65);
+
+            }
+            else if (97 <= pou && pou <= 122){
+                zero = (pou - shift - 97)%26;
+                if (zero < 0){
+                    zero = 26 + zero;
+                }
+                wab += (char) (zero + 97);
+
+            }
+            else {
+                wab += pou;
+            }
+
+        }
+        return wab;
+
+        
     }
 
 
--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -7,8 +7,37 @@ public class Vigenere {
     }
 
     public static String decryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int zero = 0;
+        String tre = "";
+        for (int k = 0; k < message.length(); k++){
+            int shift = key.charAt(zero)-65;
+            int gba = message.charAt(k);
+            if (zero == key.length()){
+                zero = 0;
+
+            }
+            if (gba < 91 && gba > (shift + 64)||(gba < 123 && gba > (shift + 96))){
+                tre += (char) (gba - shift);
+                zero += 1;
+
+            }
+            else if (gba < 91 && gba > 64){
+                tre += (char) (91 - shift + (gba%65));
+                zero += 1;
+            
+            }
+            else if (gba < 123 && gba > 96){
+                tre += (char) (123 - shift + (gba%97));
+                zero += 1;
+            }
+            else {
+                tre += gba;
+            }
+            
+
+        }
+        return tre;
+        
     }
 
 
