d0a3ada6cd885bd46f0b16d3c593b4a3e0a73680
Kiran-O <22kokelly@athenian.org>
2020-09-23 02:51:19 +0000

    Caeser Cipher works Vigenere does not fully work yet

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -2,8 +2,36 @@ import java.util.Scanner;
 
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        int zero = 0;
+        String tre = "";
+        for (int k = 0; k < message.length(); k++){
+            int shift = key.charAt(zero)+65;
+            int gba = message.charAt(k);
+            if (zero == key.length()){
+                zero = 0;
+
+            }
+            if (gba < 91 && gba > (shift + 64)||(gba < 123 && gba > (shift + 96))){
+                tre += (char) (gba + shift);
+                zero += 1;
+
+            }
+            else if (gba < 91 && gba > 64){
+                tre += (char) (91 + shift + (gba%65));
+                zero += 1;
+            
+            }
+            else if (gba < 123 && gba > 96){
+                tre += (char) (123 + shift + (gba%97));
+                zero += 1;
+            }
+            else {
+                tre += gba;
+            }
+            
+
+        }
+        return tre;
     }
 
     public static String decryptVigenere(String message, String key) {
